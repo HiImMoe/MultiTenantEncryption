c892be5841919c97bb27235901981b44498dd953
Moritz Flügel <m.fluegel@tiffinger-thiel.de>
2022-12-18 15:32:36 +0100

    change read me

--- mt-middleware/src/service/hash.service.ts
+++ mt-middleware/src/service/hash.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { scryptSync } from 'crypto';
 
+const SCRYPT_PARAMS = {
+  N: 2048,
+  r: 8,
+  p: 1,
+};
+
 export interface HashKeys {
   key: string;
   numberOfLetters?: number;
@@ -11,11 +17,12 @@ export class HashService {
   hashSearchParams(object: object, salt: string) {
     const hashParams = {};
     for (const [key] of Object.entries(object)) {
-      hashParams[key] = scryptSync(object[key].toString(), salt, 64, {
-        N: 1024,
-        r: 1,
-        p: 1,
-      }).toString('hex');
+      hashParams[key] = scryptSync(
+        object[key].toString(),
+        salt,
+        64,
+        SCRYPT_PARAMS,
+      ).toString('hex');
     }
     return hashParams;
   }
@@ -25,20 +32,22 @@ export class HashService {
     hashKeys.forEach((hashKey) => {
       if (object[hashKey.key]) {
         hashObject[`${hashKey.key}_bi`] = [
-          scryptSync(object[hashKey.key].toString(), salt, 64, {
-            N: 1024,
-            r: 1,
-            p: 1,
-          }).toString('hex'),
+          scryptSync(
+            object[hashKey.key].toString(),
+            salt,
+            64,
+            SCRYPT_PARAMS,
+          ).toString('hex'),
         ];
         if (hashKey.numberOfLetters) {
           for (let i = 0; i < hashKey.numberOfLetters; i++) {
             hashObject[`${hashKey.key}_bi`].push(
-              scryptSync(object[hashKey.key].substring(0, i + 1), 'salt', 64, {
-                N: 1024,
-                r: 1,
-                p: 1,
-              }).toString('hex'),
+              scryptSync(
+                object[hashKey.key].substring(0, i + 1),
+                salt,
+                64,
+                SCRYPT_PARAMS,
+              ).toString('hex'),
             );
           }
         }
--- readme.md
+++ readme.md
@@ -1,11 +1,13 @@
-## Start Multi-Tenant Encryption System
+## General start up
 
 install dependencies:
 `yarn`
 
 start docker container:
--> first check the docker-compose file. For m1 laptops use wizzn/keycloak:14 image for other use jboss/keycloak:12.0.4 image
+-> first check the docker-compose file. For mac m1 laptops use wizzn/keycloak:14 image for other use jboss/keycloak:12.0.4 image
 `docker-compose up -d`
+
+## Start Multi-Tenant Encryption System
 
 start encryption backend:
 open new terminal
@@ -20,7 +22,7 @@ start key management service
 
 import data:
 go to http://localhost:3000/api
-and call the import api route
+and call the /import api route
 
 ## Start classic Multi-Tenant System
 
@@ -32,10 +34,18 @@ start backend
 
 import data:
 go to http://localhost:3300/api
-and call the import api route
+and call the /import api route
 
 ## Test Setup
 
 - install JMeter from https://jmeter.apache.org/
 - open JMeter and load config from project root
+- check all paths to the files and adjust if needed (the naming of the components is always CSV ...)
+
+- SETTINGS:
+
+  - To adjust the number of users open the "Thread Group" component and adjust the "Number of Threads (users)" variable (max is 100)
+  - To adjust the number of employees created per tenant open the component "02 Loop Controller"
+  - To adjust the number of boni, missing days and performance ratings created per employee open the component "02-03 Loop Controller"
+
 - before starting a test always clear settings and db with `docker-compose down` and `docker-compose up -d`
